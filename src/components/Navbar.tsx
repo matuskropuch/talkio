ea599da10dbde986cd51fd5a86a5dfd677d5e461
Matus Kropuch <kropuchm@gmail.com>
2018-10-23 14:51:15 +0200

    navbar does not shrink with more messages displayed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import { ProfileButton } from './ProfileButton';
 export class Navbar extends React.PureComponent {
   render(): JSX.Element {
     return (
-      <nav className="navbar navbar-expand navbar-dark bg-dark">
+      <nav className="navbar navbar-expand navbar-dark bg-dark flex-shrink-0">
         <a className="navbar-brand" href="#">Talkio</a>
 
         <div className="navbar-nav ml-auto">
