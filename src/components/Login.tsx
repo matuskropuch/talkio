ca565a3905b69e80b0a8713fbfaa1891f32f17ae
Matus Kropuch <kropuchm@gmail.com>
2018-12-18 23:20:07 +0100

    Preventing submission of login form

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -5,12 +5,16 @@ interface ILoginProps {
 }
 
 export class Login extends React.PureComponent<ILoginProps, {}> {
+  onFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className="row h-100 w-100">
         <div className="jumbotron my-auto mx-auto w-50">
           <h1 className="display-4">Welcome to Talkio!</h1>
-          <form className="" action="post">
+          <form onSubmit={this.onFormSubmit} action="#" method="post">
             <div className="form-group">
               <label htmlFor="emailInput">Email</label>
               <input className="form-control" type="email" name="emailInput" id="emailInput"/>
