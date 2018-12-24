3b0a63ed6ed2103593b373dc3327c8caed1d51e7
Matus Kropuch <kropuchm@gmail.com>
2018-12-24 16:50:26 +0100

    Created new app

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,4 +1,4 @@
-export const appId = 'c49e3fa6-00d3-4150-bef5-97424f09d62e';
+export const appId = '4190b623-acee-4a60-9abd-cd038c25668f';
 export const baseUrl = `https://pv247messaging.azurewebsites.net/api/v2/app/${appId}`;
 export const userBaseUrl = `https://pv247messaging.azurewebsites.net/api/v2/${appId}`;
 export const authUrl = 'https://pv247messaging.azurewebsites.net/api/v2/auth';
